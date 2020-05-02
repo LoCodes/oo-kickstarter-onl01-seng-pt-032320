e033382e01d1e98e6744abf819e250a10acfd052
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 02:32:03 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,15 @@
 # Project - ::new
 #   takes a title on initialization, accessible through an attribute reader (FAILED - 1)
 #   initializes with a @backers attribute, an empty array
+
+
+class Project
+  attr_reader :name
+  attr_accessor :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+end
