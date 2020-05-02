09909bc1f78011199d63914ee97bc80885873f1a
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 03:52:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -15,6 +15,9 @@ class Backer
   #back_project
   # accepts a Project as an argument and stores it in a backed_projects array
 
+  # #More Advanced #back_project
+  # also adds the backer to the project's backers array
+
   def back_project(project)
     backed_projects << project
     project.backers << self
