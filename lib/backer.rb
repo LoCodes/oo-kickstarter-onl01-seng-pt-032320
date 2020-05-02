5114b4f8ece1bece40fce7e0c8fc8cc221266b06
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 02:24:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,10 @@
 # Backer - ::new
 #   takes a name on initialization, accessible through an attribute reader (FAILED - 1)
 #   initializes with a @backed_projects attribute, an empty array
+
+
+class Backer
+  attr_accessor :name
+
+
+end
