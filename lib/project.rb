9f0d03cf9acb4d58adf241f2e1d9616c2501ad48
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 02:31:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,2 +1,3 @@
 # Project - ::new
-#   takes a title on initialization, accessible through an attribute reader
+#   takes a title on initialization, accessible through an attribute reader (FAILED - 1)
+#   initializes with a @backers attribute, an empty array
